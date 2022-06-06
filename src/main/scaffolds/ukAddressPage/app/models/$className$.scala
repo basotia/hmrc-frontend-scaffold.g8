8a427f442b31acc071a42f9a5eccdb51fa4ffbb2
manish basotia <manish.basotia@sky.uk>
2022-06-06 13:04:11 +0100

    address json format fixes

--- src/main/scaffolds/ukAddressPage/app/models/$className$.scala
+++ src/main/scaffolds/ukAddressPage/app/models/$className$.scala
@@ -13,22 +13,22 @@ case class $className$(
                         $field1Name$: String,//line1
                         $field2Name$: Option[String],//line2
                         $field3Name$: String,//townOrCity
-                        $field4Name$: Option[String],//country
+                        $field4Name$: Option[String],//county
                         $field5Name$: String//postCode
                     )  {
 
   implicit val format = Json.format[$className$]
   val country: Country = Country("GB", "United Kingdom")
 
-  def apply( line1: String,
-             line2: Option[String],
-             townOrCity: String,
-             county: Option[String],
-             postCode: String): $className$ = new $className$(normaliseSpaces(line1),
-    normaliseSpaces(line2),
-    normaliseSpaces(townOrCity),
-    normaliseSpaces(county),
-    normaliseSpaces(postCode))
+  def apply( $field1Name$: String,
+             $field2Name$: Option[String],
+             $field3Name$: String,
+             $field4Name$: Option[String],
+             $field5Name$: String): $className$ = new $className$(normaliseSpaces($field1Name$),
+    normaliseSpaces($field2Name$),
+    normaliseSpaces($field3Name$),
+    normaliseSpaces($field4Name$),
+    normaliseSpaces($field5Name$))
 
 }
 
@@ -42,11 +42,11 @@ object $className$ {
         if (t == "GB") Reads(_ => JsSuccess(t)) else Reads(_ => JsError("countryCode must be GB"))
     }.andKeep(
       (
-        (__ \ "line1").read[String].map(normaliseSpaces) and
-          (__ \ "line2").readNullable[String].map(normaliseSpaces) and
-          (__ \ "townOrCity").read[String].map(normaliseSpaces) and
-          (__ \ "county").readNullable[String].map(normaliseSpaces) and
-          (__ \ "postCode").read[String].map(normaliseSpaces)
+        (__ \ $field1Name$).read[String].map(normaliseSpaces) and
+          (__ \ $field2Name$).readNullable[String].map(normaliseSpaces) and
+          (__ \ $field3Name$).read[String].map(normaliseSpaces) and
+          (__ \ $field4Name$).readNullable[String].map(normaliseSpaces) and
+          (__ \ $field5Name$).read[String].map(normaliseSpaces)
         )($className$(_, _, _, _, _)
       ))
   }
@@ -54,14 +54,14 @@ object $className$ {
   implicit val writes: OWrites[$className$] = new OWrites[$className$] {
 
     override def writes(o: $className$): JsObject = {
-      val line2Obj = o.line2.map(x => Json.obj("line2" -> x)).getOrElse(Json.obj())
-      val countyObj = o.county.map(x => Json.obj("county" -> x)).getOrElse(Json.obj())
+      val line2Obj = o.$field2Name$.map(x => Json.obj($field2Name$ -> x)).getOrElse(Json.obj())
+      val countyObj = o.$field4Name$.map(x => Json.obj($field4Name$ -> x)).getOrElse(Json.obj())
 
       Json.obj(
-        "line1" -> o.$field1Name$,
-        "townOrCity" -> o.$field3Name$,
+        $field1Name$ -> o.$field1Name$,
+        $field3Name$ -> o.$field3Name$,
         "country" -> o.country,
-        "postCode" -> o.$field5Name$
+        $field5Name$ -> o.$field5Name$
       ) ++ line2Obj ++ countyObj
     }
   }
